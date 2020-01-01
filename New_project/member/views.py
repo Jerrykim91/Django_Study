b5a454e14153d82e0df09bcec64ea227d89552ae
HYOWON KIM <52520780+Jerrykim91@users.noreply.github.com>
2020-01-01 16:41:17 +0900

    Django_Study(Create history)_2020.01.01

--- New_project/member/views.py
+++ New_project/member/views.py
@@ -2,14 +2,19 @@
 # import => 임포트 하기전에 뭐가 구성될지 생각해보자 
 
 from django.shortcuts import render
+# HttpResponse 클래스를 django.http에서 import합니다. 
+# 페이지를 웹 서버로 전달하기 위해서 이 클래스를 사용
 from django.http import HttpResponse
+
 
 # Create your views here.
 
 
 # main_page
 # request는 어디에서부터 오는지 알아야 겠어 
+# request =  사용자가 입력한 값과 기타 정보를 담고 있음 (GET, POST, request,등등..)
 def main_page(request):
+    #output은 html코드로 HttpResponse에 담겨 전달 
     output = '''
 <html>
 <head><title>%s</title></head>
